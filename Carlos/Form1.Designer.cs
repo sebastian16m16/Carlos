91ce4fd88da3b5f7113bbbde136a9e39f64fe0b7
Sebastian <sebastian16m16@gmail.com>
2020-06-05 19:51:01 +0300

    Finished

--- Carlos/Form1.Designer.cs
+++ Carlos/Form1.Designer.cs
@@ -34,13 +34,17 @@
             this.label2 = new System.Windows.Forms.Label();
             this.TeamsTable = new System.Windows.Forms.DataGridView();
             this.RESETbtn = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.deleteAll = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.TeamsTable)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(139, 25);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(23, 20);
@@ -51,19 +55,24 @@
             // 
             // GenerateBtn
             // 
+            this.GenerateBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.GenerateBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.GenerateBtn.Location = new System.Drawing.Point(665, 415);
             this.GenerateBtn.Name = "GenerateBtn";
             this.GenerateBtn.Size = new System.Drawing.Size(75, 23);
             this.GenerateBtn.TabIndex = 3;
             this.GenerateBtn.Text = "Generate";
-            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.UseVisualStyleBackColor = false;
             this.GenerateBtn.Click += new System.EventHandler(this.GenerateBtn_Click);
             // 
             // TextBoxNrOfTeams
             // 
+            this.TextBoxNrOfTeams.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.TextBoxNrOfTeams.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TextBoxNrOfTeams.Location = new System.Drawing.Point(665, 45);
             this.TextBoxNrOfTeams.Name = "TextBoxNrOfTeams";
-            this.TextBoxNrOfTeams.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxNrOfTeams.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.TextBoxNrOfTeams.Size = new System.Drawing.Size(100, 21);
             this.TextBoxNrOfTeams.TabIndex = 4;
             // 
             // label2
@@ -77,6 +86,7 @@
             // 
             // TeamsTable
             // 
+            this.TeamsTable.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.TeamsTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.TeamsTable.Location = new System.Drawing.Point(245, 95);
             this.TeamsTable.Name = "TeamsTable";
@@ -85,6 +95,7 @@
             // 
             // RESETbtn
             // 
+            this.RESETbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.RESETbtn.Location = new System.Drawing.Point(13, 415);
             this.RESETbtn.Name = "RESETbtn";
             this.RESETbtn.Size = new System.Drawing.Size(75, 23);
@@ -93,22 +104,43 @@
             this.RESETbtn.UseVisualStyleBackColor = true;
             this.RESETbtn.Click += new System.EventHandler(this.RESETbtn_Click);
             // 
-            // button2
+            // deleteAll
             // 
-            this.button2.Location = new System.Drawing.Point(112, 415);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(96, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "DELETE ALL";
-            this.button2.UseVisualStyleBackColor = true;
+            this.deleteAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteAll.Location = new System.Drawing.Point(112, 415);
+            this.deleteAll.Name = "deleteAll";
+            this.deleteAll.Size = new System.Drawing.Size(96, 23);
+            this.deleteAll.TabIndex = 8;
+            this.deleteAll.Text = "DELETE ALL";
+            this.deleteAll.UseVisualStyleBackColor = true;
+            this.deleteAll.Click += new System.EventHandler(this.deleteAll_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(195, 263);
+            this.panel1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Niagara Solid", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(311, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 36);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "CARLOS Generator";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.deleteAll);
             this.Controls.Add(this.RESETbtn);
             this.Controls.Add(this.TeamsTable);
             this.Controls.Add(this.label2);
@@ -131,7 +163,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridView TeamsTable;
         private System.Windows.Forms.Button RESETbtn;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button deleteAll;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Carlos/Form1.cs
+++ Carlos/Form1.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Xml;
 
 namespace Carlos
 {
@@ -17,7 +17,9 @@ namespace Carlos
         Logic.PersonListManagement listManagement;
         List<TextBox> textBoxes;
         List<Button> buttons;
+        List<Label> labels;
         static int x;
+        Font font;
 
         public Form1()
         {
@@ -25,6 +27,17 @@ namespace Carlos
             this.textBoxes = new List<TextBox>();
             this.listManagement = new Logic.PersonListManagement();
             this.buttons = new List<Button>();
+            this.labels = new List<Label>();
+
+            panel1.AutoScroll = false;
+            panel1.HorizontalScroll.Enabled = false;
+            panel1.HorizontalScroll.Visible = false;
+            panel1.HorizontalScroll.Maximum = 0;
+            panel1.AutoScroll = true;
+
+            font = new Font("Times New Roman", 10);
+
+            this.AcceptButton = this.GenerateBtn;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,36 +49,102 @@ namespace Carlos
             if(textBoxes.Count() == 0)
             {
                 textBoxes.Add(newTextBox);
-                newTextBox.Location = new Point(30 , 50);
-                this.Controls.Add(newTextBox);
+                newTextBox.Location = new Point(30 , 0);
+                newTextBox.Name = x + "_txtBox";
+                this.panel1.Controls.Add(newTextBox);
 
                 newLabel.Text = x.ToString() + ".";
                 newLabel.Location = new Point(newTextBox.Location.X - 15, newTextBox.Location.Y + 6);
-                this.Controls.Add(newLabel);
+                labels.Add(newLabel);
+                this.panel1.Controls.Add(newLabel);
 
                 minusBtn.Text = "-";
-                minusBtn.Location = new Point(140, button1.Location.Y + 25);
+                minusBtn.Location = new Point(140, newTextBox.Location.Y);
                 minusBtn.Size = button1.Size;
+                minusBtn.Name = x + "_btn";
                 this.buttons.Add(minusBtn);
-                this.Controls.Add(minusBtn);
+                this.panel1.Controls.Add(minusBtn);
+                minusBtn.FlatStyle = FlatStyle.Flat;
+                minusBtn.BackColor = Color.FromArgb(173, 1, 5);
+                minusBtn.Font = font;
+                minusBtn.MouseClick += MinusBtn_MouseClick;
             }
             else
             {
                 Point point = textBoxes[textBoxes.Count - 1].Location;
                 newTextBox.Location = new Point(point.X, point.Y + 25);
+                newTextBox.Name = x + "_txtBox";
                 textBoxes.Add(newTextBox);
-                this.Controls.Add(newTextBox);
+                this.panel1.Controls.Add(newTextBox);
 
                 newLabel.Text = x.ToString() + ".";
                 newLabel.Location = new Point(newTextBox.Location.X - 15, newTextBox.Location.Y + 6);
-                this.Controls.Add(newLabel);
+                labels.Add(newLabel);
+                this.panel1.Controls.Add(newLabel);
 
                 Point btnPoint = buttons[buttons.Count - 1].Location;
                 minusBtn.Text = "-";
                 minusBtn.Location = new Point(140, btnPoint.Y + 25);
                 minusBtn.Size = button1.Size;
                 buttons.Add(minusBtn);
-                this.Controls.Add(minusBtn);
+                minusBtn.Name = x + "_btn";
+                this.panel1.Controls.Add(minusBtn);
+                minusBtn.FlatStyle = FlatStyle.Flat;
+                minusBtn.BackColor = Color.FromArgb(173, 1, 5);
+                minusBtn.Font = font;
+                minusBtn.MouseClick += MinusBtn_MouseClick;
+            }
+        }
+
+        private void MinusBtn_MouseClick(object sender, MouseEventArgs e)
+        {
+            int counter = 0;
+            Point newButtonPoint;
+            Point newTextBoxPoint;
+            Point newLabelPoint;
+            foreach(Button b in buttons)
+            {
+                if (b.Equals((Button)sender))
+                {
+                    newButtonPoint = b.Location;
+                    counter = (int)Char.GetNumericValue(b.Name[0]);
+                    this.panel1.Controls.Remove(buttons[counter-1]);
+                    buttons.RemoveAt(counter-1);
+
+                    newTextBoxPoint = textBoxes[counter - 1].Location;
+                    this.panel1.Controls.Remove(textBoxes[counter-1]);
+                    textBoxes.RemoveAt(counter-1);
+
+                    newLabelPoint = labels[counter - 1].Location;
+                    this.panel1.Controls.Remove(labels[counter-1]);
+                    labels.RemoveAt(counter-1);
+                    x--;
+
+                    for (int i = (buttons.Count-1); i > counter-1; i--)
+                    {
+                        buttons[i].Name = i + 1 + "_btn";
+                        buttons[i].Location = buttons[i-1].Location;
+
+                        textBoxes[i].Name = i + 1 + "_textBox";
+                        textBoxes[i].Location = textBoxes[i-1].Location;
+
+                        labels[i].Text = i + 1 + ".";
+                        labels[i].Location = labels[i-1].Location;
+                    }
+
+                    if(counter <= buttons.Count)
+                    {
+                        buttons[counter - 1].Name = counter + "_btn";
+                        buttons[counter - 1].Location = newButtonPoint;
+
+                        textBoxes[counter - 1].Name = counter + "_textBox";
+                        textBoxes[counter - 1].Location = newTextBoxPoint;
+
+                        labels[counter - 1].Text = counter + ".";
+                        labels[counter - 1].Location = newLabelPoint;
+                    }
+                    break;
+                }
             }
         }
 
@@ -74,8 +153,12 @@ namespace Carlos
             listManagement.REMOVE_ALL();
             TeamsTable.Rows.Clear();
             TeamsTable.Columns.Clear();
+            int noOfTeams = 0;
 
-            if(TextBoxNrOfTeams.Text != "")
+            if (TextBoxNrOfTeams.Text != "")
+                noOfTeams = Int32.Parse(TextBoxNrOfTeams.Text);
+
+            if( noOfTeams > 0)
             {
                 if(Int32.Parse(TextBoxNrOfTeams.Text) <= textBoxes.Count)
                 {
@@ -118,6 +201,18 @@ namespace Carlos
             TeamsTable.Refresh();
         }
 
-
+        private void deleteAll_Click(object sender, EventArgs e)
+        {
+            for(int i=0; i<buttons.Count; i++)
+            {
+                this.panel1.Controls.Remove(buttons[i]);
+                this.panel1.Controls.Remove(textBoxes[i]);
+                this.panel1.Controls.Remove(labels[i]);
+            }
+            buttons.Clear();
+            textBoxes.Clear();
+            labels.Clear();
+            x = 0;
+        }
     }
 }
